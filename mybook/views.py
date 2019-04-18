4d42bfe5933bad21778c510483e65648381cb226
Polina Takhteeva <t-polya@mail.ru>
2019-04-18 09:23:57 +0300

    show user books

--- mybook/views.py
+++ mybook/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+import requests
+
+EMAIL = 'some_mail@mail.ru'
+PASSWARD = 'password'
+URL_AUTH = "https://mybook.ru/api/auth/"
+URL_BOOK_USER_LIST = "https://mybook.ru/api/bookuserlist/"
+HEADERS = {'Accept': 'application/json; version=5'}
 
 def books_list(request):
-	books = []
+	session = requests.Session()
+	resp = session.post(URL_AUTH, 
+						data = {
+                        	'email': EMAIL,
+                        	'password': PASSWARD})
+	books_list_resp = session.get(URL_BOOK_USER_LIST,
+                     headers=HEADERS)
+	books = get_books_resp(books_list_resp)
 	return render(
         request, 'mybook/books.html',
         {'books': books}
     )
+
+def get_books_resp(books_list_resp):
+	books = []
+	for obj in books_list_resp.json()['objects']:
+		book = {k: v for k, v in obj['book'].items() if k in ('name', 'authors_names', 'default_cover')}
+		books.append(book)
+	return books
