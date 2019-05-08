04cb238927459ca2735534fe07dfbd2774b984bd
Polina Takhteeva <t-polya@mail.ru>
2019-05-08 13:33:11 +0300

    decorator test

--- mybook/tests.py
+++ mybook/tests.py
@@ -1,22 +1,39 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.http import HttpRequest
+from mock import Mock
 import json
 
-from mybook.views import books_list, get_books_resp, login
-from mybook import test_data
+from mybook import views, test_data
 
 class GetBooksFromRespTest(TestCase):
 
 	def test_empty_resp(self):
 		json_empty_resp = json.loads(test_data.EMPTY_RESP)
-		self. assertEquals(get_books_resp(json_empty_resp), [])
+		self.assertEquals(views.get_books_resp(json_empty_resp), [])
 
 	def test_one_book_resp(self):
 		json_one_book_resp = json.loads(test_data.ONE_BOOK_RESP)
-		self.assertEquals(get_books_resp(json_one_book_resp), test_data.ONE_BOOK_DATA)
+		self.assertEquals(views.get_books_resp(json_one_book_resp), test_data.ONE_BOOK_DATA)
 
 	def test_books_resp(self):
 		json_books_resp = json.loads(test_data.BOOKS_RESP)
-		self.assertEquals(get_books_resp(json_books_resp), test_data.BOOKS_DATA)
+		self.assertEquals(views.get_books_resp(json_books_resp), test_data.BOOKS_DATA)
 
 
+class TestMyBookLoginRequired(TestCase):
+
+	def test_not_auth(self):
+		func =  Mock()
+		decorated_func = views.mybook_login_requared(func)
+		request = HttpRequest()
+		resp = decorated_func(request)
+		self.assertIs(func.called, False)
+
+	def test_auth(self):
+		func =  Mock()
+		decorated_func = views.mybook_login_requared(func)
+		request = HttpRequest()
+		request.COOKIES['session'] = test_data.SESSION_COOKIE
+		resp = decorated_func(request)
+		self.assertIs(func.called, True)
